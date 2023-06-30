935b34e58fbc0822a76a2d887fd6917fba9a4cc9
kai <kai@ASUS-Ryzen.localdomain>
2023-06-30 17:07:09 +0800

    feat(pack): add Java base pack
    Support for Java lsp, parser, dap .etc

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
 }
